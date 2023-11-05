66944a734a23c2ed5e9498fce73993bfd2e2626d
dayoonasanya <aonasanya@mail.dccc.edu>
2023-11-04 17:08:40 -0700

    Main.h

---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*********************HEADER FILE**************************/
+/**
+ * File_name: __SHELL_H
+ * Auth: dayoonasanya & chifundo365
+ *
+ *ALX Project
+ */
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+struct bulletins.
+{
+	char *env;
+	char *exit;
+} bulletins;
+
+struct information
+{
+	int final_exit;
+	int ln_count;
+} information;
+
+struct flag_s
+{
+	bool interactive;
+} flag_s;
+
+#endif
