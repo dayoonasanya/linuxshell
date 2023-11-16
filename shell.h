5249f0263ff198dc1abf4cd06d42234ad129009c
Adedayo Onasanya <aonasanya@mail.dccc.edu>
2023-11-16 03:06:08 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -17,7 +17,7 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* Do command chaining */
+/* Do cmd chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
@@ -27,7 +27,7 @@
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 
-/* 1 if using system getline() */
+/* 1 if using sys getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
@@ -41,7 +41,7 @@ extern char **environ;
  * struct liststr - singly linked list
  * @num: the number field node
  * @str: a string
- * @next: pointing to the next node
+ * @next: point to the next node
  */
 typedef struct liststr
 {
@@ -117,42 +117,42 @@ int find_builtin(runtime_info_t *);
 void find_cmd(runtime_info_t *);
 void fork_cmd(runtime_info_t *);
 
-/* toem_custom_parser.c */
-int custom_is_cmd(runtime_info_t *, char *);
-char *custom_dup_chars(char *, int, int);
-char *custom_find_path(runtime_info_t *, char *, char *);
+/* toem_pass.c */
+int cuz_is_cmd(runtime_info_t *, char *);
+char *cuz_dup_chars(char *, int, int);
+char *cuz_find_path(runtime_info_t *, char *, char *);
 
 /* loophsh.c */
 int loophsh(char **);
 
-/* toem_error_handling.c */
+/* toem_printerr.c */
 void _eputs(char *);
 int _eputchar(char);
-int _putfl(char c, int fd);
-int _putsfl(char *str, int fd);
+int _putfl(char c, int fl);
+int _putsfl(char *str, int fl);
 
-/* toem_string_man.c */
+/* toem_haystack.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
 
-/* toem_string_man1.c */
+/* toem_str.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
-/* toem_string_manipulation.c */
+/* toem_str_man.c */
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
 
-/* toem_tokenize.c */
+/* toem_tokenizer.c */
 char **strtow(char *, char *);
 char **strtow2(char *, char);
 
-/* toem_realm.c */
+/* toem_realloc.c */
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
@@ -160,46 +160,46 @@ void *_realloc(void *, unsigned int, unsigned int);
 /* toem_memory.c */
 int bfree(void **);
 
-/* toem_custom_utils.c */
+/* toem_utility.c */
 int cuz_interactive(runtime_info_t *);
 int cuz_is_delim(char, char *);
 int cuz_isalpha(int);
 int cuz_atoi(char *);
 
-/* toem_error_handling2.c */
+/* toem_printerr2.c */
 int _erratoi(char *);
 void print_error(runtime_info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-/* toem_custom_builtin.c */
+/* toem_builtins.c */
 int cuz_myexit(runtime_info_t *);
 int cuz_mycd(runtime_info_t *);
 int cuz_myhelp(runtime_info_t *);
 
-/* toem_builtin2.c */
+/* toem_builtin1.c */
 int cuz_myhistory(runtime_info_t *);
 int _myalias(runtime_info_t *);
 
-/*toem_input.c */
+/*toem_inputing.c */
 ssize_t get_input(runtime_info_t *);
 int _getline(runtime_info_t *, char **, size_t *);
 void sigintHandler(int);
 
-/* toem_info.c */
+/* toem_inform.c */
 void clear_info(runtime_info_t *);
 void set_info(runtime_info_t *, char **);
 void free_info(runtime_info_t *, int);
 
-/* toem_environment_manager.c */
+/* toem_envic_mgr.c */
 char *_getenv(runtime_info_t *, const char *);
 int _myenv(runtime_info_t *);
 int _mysetenv(runtime_info_t *);
 int _myunsetenv(runtime_info_t *);
 int populate_env_list(runtime_info_t *);
 
-/* toem_environment.c */
+/* toem_envic.c */
 char **get_environ(runtime_info_t *);
 int _unsetenv(runtime_info_t *, char *);
 int _setenv(runtime_info_t *, char *, char *);
@@ -211,21 +211,21 @@ int read_history(ruuntime_info_t *info);
 int build_history_list(runtime_info_t *info, char *buf, int linecount);
 int renumber_history(runtime_info_t *info);
 
-/* toem_custom_list.c */
+/* toem_listing.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
-/* toem_custom_lists2.c */
+/* toem_listing2.c */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 
-/* toem_chaining.c */
+/* toem_chains.c */
 int cuz_is_chain(runtime_info_t *, char *, size_t *);
 void cuz_check_chain(runtime_info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(runtime_info_t *);
